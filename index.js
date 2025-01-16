af6f42c6ac096ce322ee3935ff18028ba4c12934
HEMANTSAHU123 <hs1282542@gmail.com>
2025-01-16 12:50:58 +0530

    How to Scale

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 // Write your code below:
-
 function handleFormSubmit(event) {
     event.preventDefault();
     const name = event.target.username.value;
     const email = event.target.email.value;
     const phone = event.target.phone.value;
-    const userdetails = {
-        username:name,
+    const obj = {
+        username: name,
         email: email,
         phone: phone
-    }
-    let users_serialised = JSON.stringify(userdetails)
-    localStorage.setItem("User Details", users_serialised);
 
+    }
+   
+    let userserialised = JSON.stringify(obj);
+    localStorage.setItem(email, userserialised);
+    display(obj);
+}
+function display(obj) {
+    const ul = document.querySelector('ul');
+    const creatnewli = document.createElement('li');
+    creatnewli.textContent = obj.username + obj.email + obj.phone;
+    ul.appendChild(creatnewli);
 }
 module.exports = handleFormSubmit;
