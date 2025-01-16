7e722cbefd201f1a924d6fd450adea2072508264
HEMANTSAHU123 <hs1282542@gmail.com>
2025-01-16 12:46:34 +0530

    Understanding Local Storage

--- index.js
+++ index.js
@@ -1,68 +1,16 @@
-// Add the Edit Button:
 
 
-// Implement the code as in video but with one extra 'Edit' button in 'li
-const fruitItems = document.getElementsByClassName('fruit')
-
-const description = {
-    Banana: "A long curved fruit with a yellow skin, soft flesh, and multiple seeds",
-    Apple: "A round fruit typically with red, green, or yellow skin, containing seeds in its core",
-    Orange: "A round fruit with a tough orange rind and juicy segmented flesh inside",
-    Kiwi: "A small fruit with brown fuzzy skin, green flesh, and black seeds inside"
-}
-for (const fruitItem of fruitItems) {
-    const name = fruitItem.firstChild.textContent
-
-    fruitItem.innerHTML = name + `<p>${name} : ${description[name]}</p>`
-}
-
-const form = document.querySelector('form')
-const button = form.querySelector('button')
-
-const descriptionInput = document.createElement('input')
-descriptionInput.type = 'text'
-descriptionInput.name = 'description'
-descriptionInput.id = 'description'
-
-form.insertBefore(descriptionInput, button);
-form.addEventListener('submit', (event) => {
+function handleFormSubmit (event) {
     event.preventDefault();
-
-    const fruitInput = form.querySelector('#fruit-to-add').value;
-    const description = form.querySelector('#description').value;
-
-    const fruitList = document.querySelector('.fruits')
-    const li = document.createElement('li')
-
-    li.innerHTML = fruitInput + ` <p style="font-style:italic">${description}</p>`
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'x';
-    deleteButton.className = 'delete-btn';
-    deleteButton.addEventListener('click', function () {
-        fruitList.removeChild(li);
-    });
-
-    li.appendChild(deleteButton);
-    fruitList.appendChild(li);
-
-    form.reset();
-});
-const filter = document.getElementById('filter')
-
-filter.addEventListener('keyup', (event) => {
-    const textEnterd = event.target.value.toLowerCase()
+    const username = event.target.username.value;
+    const email = event.target.email.value;
+    const phone = event.target.phone.value;
 
 
-    const fruitItems = document.getElementsByClassName('fruit')
-    for (const fruitItem of fruitItems) {
-        const currentFruitText = fruitItem.firstChild.textContent.toLocaleLowerCase()
-
-        console.log(fruitItem.lastChild.textContent)
+    localStorage.setItem('Username', username);
+    localStorage.setItem('Email', email);
+    localStorage.setItem('Phone', phone);
 
 
-        const currentFruitTextIndex = currentFruitText.indexOf(textEnterd)
-
-
-        fruitItem.style.display = (currentFruitTextIndex == -1) ? "none" : 'flex'
-    }
-})
+};
+module.exports = handleFormSubmit;
